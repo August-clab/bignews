b2f9e97b6c0880e99e0b325d0fedb8bb31e94cda
August <atop_lin@163.com>
2020-05-17 14:14:08 +0800

    个人中心图片浏览

--- admin/js/user.js
+++ admin/js/user.js
@@ -13,9 +13,28 @@ $(function () {
                 $('#form .password').val(res. data.password) */
                 for (var key in res.data) {
                     $('#form .' + key).val(res.data[key])
-                }
-                $('#form .user_pic').attr('src', res.data.userPic)
+                };
+                $('#form .user_pic').attr('src', res.data.userPic);
             }
         }
     })
+
+
+    // 个人中心实现图片预览
+    $('#exampleInputFile').on('change', function () {
+        // 获取待上传的文件
+        var file = this.files[0];
+        // URL.createObjectURL会将待上传的文件生成一个可浏览的地址
+        var url = URL.createObjectURL(file);
+        // 在图片上渲染出来，预览待上传的图片
+        $('#form .user_pic').attr('src', url) ；
+    })
+
+
+    // 更新个人中心的数据
+
+
+
+
+
 })
