c9482153b9a8e2b0539a0d74ef33b45c59398661
August <atop_lin@163.com>
2020-05-20 16:44:47 +0800

    第三天

--- admin/js/article_category.js
+++ admin/js/article_category.js
@@ -1,18 +1,26 @@
 $(function () {
     // 发送ajax请求
-    $.ajax({
-        type: 'get',
-        url: BigNew.category_list,
-        success: function (res) {
-            if (res.code == 200) {
-                var htmlStr = template('categoryList', res)
-                $('tbody').html(htmlStr)
+    render();
+    function render() {
+        $.ajax({
+            type: 'get',
+            url: BigNew.category_list,
+            success: function (res) {
+                if (res.code == 200) {
+                    var htmlStr = template('categoryList', res)
+                    $('tbody').html(htmlStr)
+                }
             }
-        }
-    })
+        })
+    }
+
+
     // 获取数据并渲染页面
     // 新增按钮注册事件弹出模态框
     $('#xinzengfenlei').on('click', function () {
+        // 清空模态框
+        $('#myform')[0].reset()//重置表单
+        // 显示模态框
         $('.addModal').modal('show');
         // 修改提示标题
         $('.addModal h4').text('新增文章分类');
@@ -26,8 +34,77 @@ $(function () {
         $('.addModal').modal('show');
         // 修改提示标题
         $('.addModal h4').text('更新文章分类');
-
+        // 发送ajax请求
+        $.ajax({
+            type: 'get',
+            url: BigNew.category_search,
+            data: {
+                id: $(this).data('id')
+            },
+            success: function (res) {
+                if (res.code == 200) {
+                    // 将查询到的要编辑的数据线显示在模态框上
+                    $('#myform input[name=id]').val(res.data[0].id)
+                    $('#myform input[name=name]').val(res.data[0].name)
+                    $('#myform input[name=slug]').val(res.data[0].slug)
+                }
+            }
+        })
     })
+
+
+    // 新增或更新数据
+    // 给模态框的确定按钮注册事件
+    // 根据id有没有数据进行判断 是新增还是更新操作
+    // 如果成功刷新页面
+    $('.addModal .btn-sure').on('click', function () {
+        // 获取隐藏域中的id
+        var id = $('#myform input[name=id]').val()
+        // 发送ajax请求
+        $.ajax({
+            type: 'post',
+            url: id ? BigNew.category_edit : BigNew.category_add,
+            data: $('#myform').serialize(),
+            success: function (res) {
+                if (res.code == 200 || res.code == 201) {
+                    // 隐藏模态框
+                    $('.addModal').modal('hide')
+                    //刷新页面
+                    render()
+                }
+            }
+        })
+    })
+
+
+
+
+    // 删除数据
+    // 给删除按钮注册事件
+    $('tbody').on('click', '.btn-del', function () {
+        // 弹出提示框
+        $('.delModal').modal('show');
+        // 获取当前按钮所在数据的id
+        window.categoryId = $(this).data('id');
+    })
+
+    // 给删除的模态框中的确定按钮注册事件
+    $('.delModal .btn-sure').on('click', function () {
+        $.ajax({
+            type: 'post',
+            url: BigNew.category_delete,
+            data: {
+                id: window.categoryId
+            },
+            success: function (res) {
+                if (res.code == 204) {
+                    $('.delModal').modal('hide')
+                    render()
+                }
+            }
+        })
+    })
+
 
 
 
---
+++ admin/js/article_list.js
@@ -0,0 +1,100 @@
+$(function () {
+    $.ajax({
+        type: 'get',
+        url: BigNew.category_list,
+        success: function (res) {
+            // console.log(res);
+            var htmlStr = template('categoryList', res)
+
+            $('#selCategory').html(htmlStr)
+        }
+    })
+
+
+    // 显示当前页面中的所有的文章数据
+    /* $.ajax({
+        type: 'get',
+        url: BigNew.article_query,
+        data: {
+            key: $('#key').val(),
+            type: $('#selCategory').val(),
+            state: $('#selStatus').val(),
+            page: 1,
+            perpage: 7
+        },
+        success: function (res) {
+            if (res.code == 200) {
+                var htmlStr = template('articleList', res.data);
+                $('tbody').html(htmlStr)
+            }
+        }
+    }) */
+
+    // 对以上代码封装
+    getDataByParams({
+        key: $('#key').val(),
+        type: $('#selCategory').val(),
+        state: $('#selStatus').val(),
+        page: 1,
+        perpage: 7
+    });
+    function getDataByParams(obj) {
+        $.ajax({
+            type: 'get',
+            url: BigNew.article_query,
+            data: obj,
+            success: function (res) {
+                if (res.code == 200) {
+                    var htmlStr = template('articleList', res.data);
+                    $('tbody').html(htmlStr);
+
+
+                    // 启动分页功能
+                    pagination(res.data.totalPages)
+                }
+            }
+        })
+    }
+
+
+    // 给筛选按钮注册事件
+    $('#btnSearch').on('click', function (e) {
+        // 阻止默认行为
+        e.preventDefault();
+        // 调用函数来获取数据
+        getDataByParams({
+            key: $('#key').val(),
+            type: $('#selCategory').val(),
+            state: $('#selStatus').val(),
+            page: 1,
+            perpage: 7
+        })
+    })
+
+
+
+    // 实现分页功能
+    function pagination(totalPages, visiblePages) {
+        $('#pagination-demo').twbsPagination({
+            totalPages: 35,
+            visiblePages: 7,
+            first: '首页',
+            last: '最后一页',
+            next: '下一页',
+            prev: '上一页',
+            initiateStarPageClick: false,//不要默认点击
+            onPageClick: function (event, page) {
+                $('#page-content').text('Page ' + page);
+                // page是当前页码
+                getDataByParams({
+                    key: $('#key').val(),
+                    type: $('#selCategory').val(),
+                    state: $('#selStatus').val(),
+                    page: page,
+                    perpage: 7
+                })
+            }
+        })
+    }
+
+})
